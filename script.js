c2aa95d3483f4e7c26947942a46c2678e43dbf5d
Saran Pariyar <spariyar05@gmail.com>
2025-06-25 17:14:39 +0545

    new design again lol

--- script.js
+++
@@ -1,7 +0,0 @@
-const navToggleBtn = document.querySelector('#nav-toggle-btn');
-const navList = document.querySelector('#nav-list');
-
-
-navToggleBtn.addEventListener('click', () => {
-    navList.classList.toggle('show-nav-list');
-});
