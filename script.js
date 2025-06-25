d2d6b6ceb9e5c53ef3909915de2f1bb7f44093e7
Saran Pariyar <spariyar05@gmail.com>
2025-06-25 18:43:56 +0545

    navbar desktop view

---
+++ script.js
@@ -0,0 +1,14 @@
+const navbar = document.querySelector('#navbar');
+
+// window.addEventListener('scroll', () => {
+
+//     if(window.scrollY > 0){
+//         navbar.classList.add("navbar-bottom-border")
+//     }
+//     else(
+//         navbar.classList.remove("navbar-bottom-border")
+//     )
+
+
+
+// })
