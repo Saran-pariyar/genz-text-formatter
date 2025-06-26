5017f4bf99f3fcac41796e52d5beeb290d4d5b8a
Saran Pariyar <spariyar05@gmail.com>
2025-06-26 22:04:04 +0545

    Voice notice - functionality left to add

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let lastScrollY = window.scrollY;
 window.addEventListener('scroll', () => {
     const currentScrollY = window.scrollY;
 
-    if(currentScrollY > lastScrollY){
+    if (currentScrollY > lastScrollY) {
         console.log("scrolling down");
         navbar.style.position = "relative";
         // navbar.style.top = "1";
 
     }
-    else{
+    else {
         navbar.style.position = "sticky";
         navbar.style.top = 0;
 
@@ -27,5 +27,30 @@ const textFormatSection = document.querySelector("#text-format-section")
 
 
 startFormattingBtn.addEventListener("click", () => {
-    textFormatSection.scrollIntoView({   behavior: "smooth" });
+    textFormatSection.scrollIntoView({ behavior: "smooth" });
 })
+
+
+
+const formatButtons = document.querySelectorAll('.format-btn');
+
+// JFF
+
+function readOutLoud(text) {
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = text;
+    speech.lang = 'en-US'; // or change it to 'en-GB', 'es-ES', etc.
+    speech.rate = 1.5; // set to 1.5 or 2 for faster speaking if needed
+    window.speechSynthesis.speak(speech);
+}
+
+formatButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const btnId = e.target.id;
+        // console.log(`Button clicked: ${btnId}`);
+        readOutLoud("Ruko jara, sabar karo. Bolana dhaka muki nahi karneka. Still functionality add gareko chaina")
+
+        // You can use a switch or if-else here based on btnId
+        // to trigger the right function
+    });
+});
