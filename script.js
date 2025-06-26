ce48188872ed6fa3911b5c2166b7e5e0ef1afcba
Saran Pariyar <spariyar05@gmail.com>
2025-06-26 21:52:11 +0545

    created responsive nav

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 const navbar = document.querySelector('#navbar');
+let lastScrollY = window.scrollY;
 
-// window.addEventListener('scroll', () => {
+window.addEventListener('scroll', () => {
+    const currentScrollY = window.scrollY;
 
-//     if(window.scrollY > 0){
-//         navbar.classList.add("navbar-bottom-border")
-//     }
-//     else(
-//         navbar.classList.remove("navbar-bottom-border")
-//     )
+    if(currentScrollY > lastScrollY){
+        console.log("scrolling down");
+        navbar.style.position = "relative";
+        // navbar.style.top = "1";
+
+    }
+    else{
+        navbar.style.position = "sticky";
+        navbar.style.top = 0;
 
 
+    }
 
-// })
+
+    lastScrollY = currentScrollY;
+
+})
+
+const startFormattingBtn = document.querySelector("#start-formatting-btn")
+const textFormatSection = document.querySelector("#text-format-section")
+
+
+startFormattingBtn.addEventListener("click", () => {
+    textFormatSection.scrollIntoView({   behavior: "smooth" });
+})
